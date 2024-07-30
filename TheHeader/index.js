c31b3403923a0135da9e88f79e27e39e1b4bde63
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-07-30 16:23:46 +0300

    Dosyalar düzenlendi

--- TheHeader/index.js
+++
@@ -1,8 +0,0 @@
-function openMobileMenu() {
-  document.getElementById("mobileMenu").style.width = "100%";
-  document.getElementById("mobileMenu").style.backgroundColor =
-    "rgba(0,0,0,0.4)";
-}
-function closeMobileMenu() {
-  document.getElementById("mobileMenu").style.width="0";
-}
