575c1239609c2cd3741813c86702b15d84db85b9
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-07-30 16:04:30 +0300

    Codes added

---
+++ TheHeader/index.js
@@ -0,0 +1,8 @@
+function openMobileMenu() {
+  document.getElementById("mobileMenu").style.width = "100%";
+  document.getElementById("mobileMenu").style.backgroundColor =
+    "rgba(0,0,0,0.4)";
+}
+function closeMobileMenu() {
+  document.getElementById("mobileMenu").style.width="0";
+}
