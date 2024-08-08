c5be728a5aa0903b5366d7b195f3692b77c30b9d
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-08-08 22:24:56 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,93 +1,100 @@
-export  const iconsArray = [
-    {
-      iconPath: "./assets/icons/cssIcon.svg",
-    },
-    {
-      iconPath: "./assets/icons/htmlIcon.svg",
-    },
-  
-    {
-      iconPath: "./assets/icons/jsIcon.svg",
-    },
-  
-    {
-      iconPath: "./assets/icons/sassIcon.svg",
-    },
-  
-    {
-      iconPath: "./assets/icons/vuejsIcon.svg",
-    },
-  ];
-  export  const cardsArray = [
-    {
-      id: 0,
-      title: "Diamond",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/diamond2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/Diamond/",
-    },
-    {
-      id: 1,
-      title: "Frog",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/frog2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/Frog/",
-    },
-  
-    {
-      id: 2,
-      title: "Invert Color",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/invertColor2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/LightText/",
-    },
-    {
-      id: 3,
-      title: "Responsive Navbar",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/navbar.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/TheHeader/",
-    },
-    {
-      id: 4,
-      title: "Rotating Squares",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/rotatingSquares2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/RotatingSquares/",
-    },
-    {
-      id: 5,
-      title: "Basic Shapes",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/shapes2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/Shapes/",
-    },
-    {
-      id: 6,
-      title: "Tetris Blocks",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/tetrisBlocks2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/TetrisBlocks/",
-    },
-    {
-      id: 7,
-      title: "Intersection",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/transaction.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/intersection/",
-    },
-    {
-      id: 8,
-      title: "Breaking News Animation",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/breakingNews-2.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/BreakingNews/",
-    },
-    {
-      id: 9,
-      title: "Product List",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
-      imageUrl: "./assets/additionalResources/patisserieImage.png",
-      link: "https://kizmazemirhan.github.io/StajCSS/ProductList/",
-    },
-  ];
+export const iconsArray = [
+  {
+    iconPath: "./assets/icons/cssIcon.svg",
+  },
+  {
+    iconPath: "./assets/icons/htmlIcon.svg",
+  },
+
+  {
+    iconPath: "./assets/icons/jsIcon.svg",
+  },
+
+  {
+    iconPath: "./assets/icons/sassIcon.svg",
+  },
+
+  {
+    iconPath: "./assets/icons/vuejsIcon.svg",
+  },
+];
+export const cardsArray = [
+  {
+    id: 0,
+    title: "Diamond",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/diamond2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/Diamond/",
+  },
+  {
+    id: 1,
+    title: "Frog",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/frog2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/Frog/",
+  },
+
+  {
+    id: 2,
+    title: "Invert Color",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/invertColor2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/LightText/",
+  },
+  {
+    id: 3,
+    title: "Responsive Navbar",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/navbar.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/TheHeader/",
+  },
+  {
+    id: 4,
+    title: "Rotating Squares",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/rotatingSquares2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/RotatingSquares/",
+  },
+  {
+    id: 5,
+    title: "Basic Shapes",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/shapes2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/Shapes/",
+  },
+  {
+    id: 6,
+    title: "Tetris Blocks",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/tetrisBlocks2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/TetrisBlocks/",
+  },
+  {
+    id: 7,
+    title: "Intersection",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/transaction.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/intersection/",
+  },
+  {
+    id: 8,
+    title: "Breaking News Animation",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/breakingNews-2.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/BreakingNews/",
+  },
+  {
+    id: 9,
+    title: "Product List",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/patisserieImage.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/ProductList/",
+  },
+  {
+    id: 10,
+    title: "Contact Form",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,dolorem debitis.",
+    imageUrl: "./assets/additionalResources/contactForm.png",
+    link: "https://kizmazemirhan.github.io/StajCSS/contactForm/",
+  },
+];
