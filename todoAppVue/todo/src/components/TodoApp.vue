0a49c4f564cee934a6791997c7afcf00d29a96b5
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-08-15 17:50:53 +0300

    project updated

--- todoAppVue/todo/src/components/TodoApp.vue
+++ todoAppVue/todo/src/components/TodoApp.vue
@@ -2,12 +2,31 @@
   <div class="panels-wrapper">
     <div class="panels-container">
       <div class="left-panel">
-        <button @click="addNote(newNote)">Add Note</button>
-        <textarea
-          type="text"
-          v-model="newNote"
-          @keyup.enter="addNote(newNote)"
-        ></textarea>
+        <h1>Lets start taking notes!</h1>
+        <div class="input-wrapper">
+          <textarea
+            type="text"
+            v-model="newNote"
+            @keyup.enter="addNote(newNote)"
+          ></textarea>
+          <button @click="addNote(newNote)">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="54"
+              height="54"
+              fill="currentColor"
+              class="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
+              />
+              <path
+                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
       <hr />
       <div class="right-panel">
